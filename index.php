b7056a967c6e530b4150f4235d41bd6bf04063bd
2tu <zhangzhenbeta@gmail.com>
2020-10-27 10:10:23 +0800

    modify index.php

--- index.php
+++ index.php
@@ -1,52 +1,51 @@
 <?php
 
-$db = "fiship.txt";
+$db = "botIPs.txt";
 $addr = $_SERVER["REMOTE_ADDR"];
-
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 
 
 if(isset($_GET['getaddr'])){
-    header('Content-type: text/json; charset=utf-8');
-    if (isset($_COOKIE['fishcookie'])){
-        echo 'var returnCitySN = {"cip": "'.$addr.'", "bobo":"111111"};';
-        }
-        else{
-            echo 'var returnCitySN = {"cip": "'.$addr.'", "bobo": "000000"};';
-        }
+        header('Content-type: text/json; charset=utf-8');
+        if (isset($_COOKIE['bobo'])){
+                echo 'var returnCitySN = {"cip": "'.$addr.'", "extend":"111111"};';
+                }
+                else{
+                        echo 'var returnCitySN = {"cip": "'.$addr.'", "extend": "000000"};';
+                }
 }
-if(isset($_GET['bite1'])){
-    setcookie("fishcookie","123321", time()+3600*240);  
+if(isset($_GET['bobo'])){
+        setcookie("bobo","bobo", time()+3600*240);
 }
 
 
 if(isset($_GET['ip'])){
-    $fishIP = @$_GET['ip'];
-    $line = file_get_contents($db);
-    $fishIPs = explode("\n", $line);
-    if (@in_array(base64_encode($fishIP), $fishIPs)) {
-        echo "ip exist";
-    }else {
-        $fishsh = fopen($db, "a") or die("Unable to open fish file!");
-        fwrite($fishsh, base64_encode($fishIP) . "\n");
-        fclose($fishsh);
-    }
+        $botIP = @$_GET['ip'];
+        $line = file_get_contents($db);
+        $botIPs = explode("\n", $line);
+        if (@in_array(base64_encode($botIP), $botIPs)) {
+                echo "ip exist";
+        }else {
+                $bots = fopen($db, "a") or die("Unable to open bots file!");
+                fwrite($bots, base64_encode($botIP) . "\n");
+                fclose($bots);
+        }
 }
 
 
 if(isset($_GET['search'])){
 
-    $ip = @$_GET['search'];
+        $ip = @$_GET['search'];
 
-    if (file_exists($db)) {
-        $line = file_get_contents($db);
-        $botIPs = explode("\n", $line);
-        if (@in_array(base64_encode($ip), $botIPs)) {
-            echo "in";
-        }else {
-            echo "not in";
+        if (file_exists($db)) {
+                $line = file_get_contents($db);
+                $botIPs = explode("\n", $line);
+                if (@in_array(base64_encode($ip), $botIPs)) {
+                        echo "in";
+                }else {
+                        echo "bobo";
+                }
         }
-    }
 }
 ?>
